8e1c24dd0df0bf5fc877fd1befbde723a2822e54
Suyang Song <suyangsong@gmail.com>
2020-09-11 15:51:19 -0400

    xcode project commit

---
+++ MasteryTracker/MasteryTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MasteryTracker
+//
+//  Created by Suyang Song on 2020-09-11.
+//  Copyright © 2020 Suyang Song. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
